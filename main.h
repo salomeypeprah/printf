72cb26389d4f9361798848b3fd1e428ffe3f67bf
salomeypeprah <sapeprah2@gmail.com>
2023-05-30 13:29:48 -0700

    this is our main.h

--- main.h
+++ main.h
@@ -1,58 +1,91 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
-#include <limits.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdarg.h>
 
-#define OUTPUT_BUF_SIZE 1024
-#define BUF_FLUSH -1
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+#define S_SHORT 1
+#define S_LONG 2
+
+#define F_SPACE 16
+#define F_ZERO 4
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_HASH 8
 
 /**
-* typedef struct format- defines a structure for symbols and functions
-*
-* @ros: a format
-* @f: The function associated with the format
-*/
-typedef struct format;
+ * struct detail - it maps the conversion specifier for printf
+ *
+ * @detail: The specifer format.
+ * @fxn: The function associated.
+ */
+struct detail
 {
-char *ros;
-int (*f)(va_list);
-}
-typedef struct format;
+	char *detail;
+	int (*fxn)(va_list, char[], int, int, int, int);
+};
 
-/*Main functions*/
+/**
+ * typedef struct detail detail_t - a function that maps the conversion specifier for printf
+ * @detail:format specifier
+ * @detail_t: function associated.
+ */
+typedef struct detail detail_t;
 
-int _putchar(char c);
-int print_char(va_list);
-int print_string(va_list);
-int print_percent(va_list);
-int print_int(va_list);
-int print_S(va_list);
-int print_unsigned(va_list);
-int address(va_list);
-int print_binary(va_list);
-int print_octal(va_list list);
-int print_hex(va_list list);
-int print_Hex(va_list list);
-unsigned int space_flag  :1
-unsigned int hastag_flag  :1
-unsigned int zero_flag   :1
-unsigned int minus_flag  :1
-unsigned int width
-unsigned int precision
-unsigned int h_modifier  :1
-unsigned int l_modifier  :1
-int print_rev(va_list.);
-int print_rot13(va_list.);
-char *convert(long int num, int base, int flag….);
+int print_int(va_list joy, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list joy, char buffer[],
+	int flags, int width, int precision, int size);
+int print_unsigned(va_list joy, char buffer[],
+	int flags, int width, int precision, int size)
+int print_char(va_list joy, char buffer[],
+	int flags, int width, int precision, int size);
+int print_rot13(va_list joy, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list joy, char buffer[],
+	int flags, int width, int precision, int size);
+int print_pointer(va_list joy, char buffer[],
+	int flags, int width, int precision, int size);
+int print_binary(va_list joy, char buffer[],
+	int flags, int width, int precision, int size);
+int print_octal(va_list joy, char buffer[],
+	int flags, int width, int precision, int size);
+int print_rever(va_list joy, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexadecimal(va_list joy, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hex(va_list joy, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
+int print_Hex_up(va_list joy, char buffer[],
+	int flags, int width, int precision, int size);
+int _printf(const char *format, ...);
+int display_non_print_char(va_list joy, char buffer[],
+	int flags, int width, int precision, int size);
+int process_print(const char *detail, int *i,
+va_list joy, char buffer[], int flags, int width, int precision, int size);
+int write_ptr(char buffer[], int ind, int len,
+	int width, int flags, char pad_char, char add_c, int pad_char_start);
+int print_num(int ind, char bff[], int flags, int width, int precision,
+	int len, char pad_char, char add_c);
+int print_number(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int process_output_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+int unsigned_to_string(int is_negative, int ind,
+char buffer[],
+	int flags, int width, int precision, int size);
+int add_hex_code(char, char[], int);
+int is_visible(char);
+int is_digit(char);
+int fetch_flags(const*format. unt *i);
+int fetch_width(const char *format, int *i, va_list joy);
+int fetch_precision(const char *format, int *i, va_list joy);
+int fetch_size(const char *format, int *i);
 
-/*Support functions*/
-unsigned int base_len(unsigned int, int);
-char *rev_string(char *);
-void write_base(char *str);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int print_unsigned_number(unsigned int);
+long int convert_size_unsigned(unsigned long int num, int size);
+long int convert_size_number(long int num, int size);
 
 #endif
